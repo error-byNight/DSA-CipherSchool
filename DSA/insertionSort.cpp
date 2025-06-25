ddaa66f78bbef92f2c6519dcaa757d39a0a7f74c
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2025-06-25 09:13:00 +0530

    dsaa

--- DSA/insertionSort.cpp
+++ DSA/insertionSort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void insertionSort(int arr[], int n){
     for (int i = 1; i<n; i++){
-        int j=1-i;
+        int j=i-1;
         int key = arr[i];
-        while(j <= 0 && arr[j] > key){
+        while(j >= 0 && arr[j] > key){
             arr[j +1] = arr[j];
             j--;
         }
