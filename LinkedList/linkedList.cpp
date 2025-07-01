b4019a36c06b018dac4d7817fee07c7889d2269b
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2025-07-01 10:34:25 +0530

    1July

---
+++ LinkedList/linkedList.cpp
@@ -0,0 +1,49 @@
+#include <iostream> 
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtEnd(Node *&head, int val) {
+    Node *n = new Node(val);
+
+    if (head == NULL) {
+        head = n;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = NULL;
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+    insertAtEnd(head, 40);
+
+    printList(head);
+
+    return 0;
+}
